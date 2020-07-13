0a26c1b3a7a01558e4c7a63aa96e6ee9fe4f7885
kompromaiz <kanin.alexey@gmail.com>
2020-07-13 23:18:07 +0700

    Added sorting for iGVault offers

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
+# for finding key by value in a dict
+
+def find_by_val(val, dict_):
+    dict_copy = dict_
+    for key, value in dict_copy.items():
+        if value == val:
+            del dict_copy[key]
+            return key
+
+# ------------------------ PlayerAuctions ------------------------
+
 PA_data = {
     'sP_ids': {'Fairbanks': 8582, 'Earthshaker': 8583},
     'server_ids': {'Fairbanks': 8751, 'Earthshaker': 9005},
-}
-
-iGVault_data = {
-    'server_id': {'Fairbanks': 2111, 'Earthshaker': 2339},
-    'faction_id': {'Fairbanks': 2236, 'Earthshaker': 2343}
 }
 
 def get_PA_data(server, stack_size):
@@ -22,24 +28,33 @@ def get_PA_data(server, stack_size):
 
 # get_PA_data('Earthshaker', 1000)
 
+# ------------------------ iGVault ------------------------
+
+iGVault_data = {
+    'server_id': {'Fairbanks': 2111, 'Earthshaker': 2339},
+    'faction_id': {'Fairbanks': 2236, 'Earthshaker': 2343}
+}
+
+# for getting a list of names for sorted prices (relevant for iGVault only)
+
+def get_sorted_names(offers, sorted_prices):
+    sorted_names = []
+    for price in sorted_prices:
+        sorted_names.append(find_by_val(price, offers))
+    return sorted_names
+
 def get_iGVault_data(server, quantity):
     url = f'https://www.igvault.com/c2c-index/list-get-other-sellers?game_id=1830&server_id={iGVault_data["server_id"][server]}&camp_id={iGVault_data["faction_id"][server]}&quantity={quantity}'
-    # url = f'https://www.igvault.com/WOW-Classic-Gold/Fairbanks-US/Alliance.html'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     page_offers_price = soup.find_all("div", {"class": "offers-us"})
     page_offers_name = soup.find_all("div", {"class": "offers-name"})
 
     offers = dict(zip([i.select("strong")[0].text for i in page_offers_name], [float(i.select("strong")[0].text[:-4]) for i in page_offers_price]))
+    sorted_offers = dict(zip([price for price in sorted(offers.values())], [name for name in get_sorted_names(offers, sorted(offers.values()))]))
 
-    # print(offers.keys(), offers.values())
-
-    for i in offers:
-        print(i, offers[i])
-
-    # for i in sorted(offers.values()):
-    #     # print(i, offers[i])
-    #     print(offers[i], i)
+    for offer in sorted_offers:
+        print(offer, sorted_offers[offer])
 
 
-get_iGVault_data('Fairbanks', 1000)
+get_iGVault_data('Earthshaker', 1000)
